7f2e5f80348fce60f42db558ad0c3658f36b8b54
Sinval Felisbert <sinvalfelisberto@gmail.com>
2020-10-24 07:42:44 -0300

    finalizado.

--- aula19/exercicio03.php
+++ aula19/exercicio03.php
@@ -12,15 +12,21 @@
     <pre>
         <?php
             $vt = array(3, 5, 8, 2);
-            // sort($vt);
+            // sort($vt); #ordena em asc - altera o indice
             print_r($vt);
 
-            // rsort($vt);
+            // rsort($vt); #ordena em desc - altera o indice
             // print_r($vt);
 
-            asort($vt);
+            // asort($vt); #ordena em asc - mantém o índice
+            arsort($vt); #ordena em desc - mantém o índice
             print_r($vt);
-
+            
+            // ksort($vt); #ordena as chaves, mantém os valores
+            // print_r($vt);
+            
+            krsort($vt);
+            print_r($vt);
             
             ?>  
         <br>
